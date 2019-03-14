e0cc66901886432c1da405cd32d63024faf7c15a
kam16u810 <bokseng12@gmail.com>
2019-03-14 14:37:10 +0300

    few edits

--- lab_06/test.lisp
+++ lab_06/test.lisp
@@ -1,1 +1,16 @@
-(write (caddr `((abc)(def)(ghi))))
+(defun f1(x lst) 
+    (if (null lst)
+        lst
+        (or (f1 x (cdr lst))
+            (if (listp (car lst))
+                (f1 x (car lst))
+                (if (equal x (car lst))
+                    t
+                    nil
+                )
+            )
+        )
+    )
+)
+
+(write (f1 1 (list 9 (list 5 1 2))))
